9920776127f069920755b85f0bfcbebb7078718c
yuki <mansie.torsholt@gmail.com>
2021-03-12 16:12:40 +0100

    select one pokemon by id

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -11,4 +11,21 @@ router.get ('/', (req,res)=>{
 })
 
 
+// get a one pokemon by id
+
+router.get('/:id', (req, res)=>{
+    const {id}=req.params;
+    const numberId=Number(id);
+    /*console.log(req.params);
+    console.log(id);
+    console.log(numberId);
+    console.log(pokedata[0].id);*/
+    const selectedPokemon = pokedata.find((pokemon)=>
+    {return  pokemon.id === numberId }
+    )
+
+    if (selectedPokemon) {res.json(selectedPokemon);}
+    else {res.sendStatus(404)}
+})
+
 module.exports= router;
